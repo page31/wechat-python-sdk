44dd086f153ce68e1ed5c2c27460b1c703a9daea
huangdong <huangdong@dayuwuxian.com>
2016-12-03 21:36:20 +0800

    修复 aes 加密填充的 bug

--- wechat_sdk/lib/crypto/base.py
+++ wechat_sdk/lib/crypto/base.py
@@ -28,8 +28,9 @@ class BaseCrypto(object):
         @param text: 需要加密的明文
         @return: 加密得到的字符串
         """
+        text = to_binary(text)
         # 16位随机字符串添加到明文开头
-        text = self.get_random_str() + struct.pack("I", socket.htonl(len(text))) + to_binary(text) + appid
+        text = self.get_random_str() + struct.pack("I", socket.htonl(len(text))) + text + appid
         # 使用自定义的填充方式对明文进行补位填充
         pkcs7 = PKCS7Encoder()
         text = pkcs7.encode(text)
